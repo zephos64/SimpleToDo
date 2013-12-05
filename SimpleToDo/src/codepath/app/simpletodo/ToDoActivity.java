d7dc09e6d9c00d8c21beccfcf9fc63c2c6315ac0
ryan <ryanwhitworth@sbcglobal.net>
2013-12-04 21:14:13 -0800

    added button functionality, added add item to list

--- SimpleToDo/src/codepath/app/simpletodo/ToDoActivity.java
+++ SimpleToDo/src/codepath/app/simpletodo/ToDoActivity.java
@@ -1,17 +1,72 @@
 package codepath.app.simpletodo;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class ToDoActivity extends Activity {
+	private ArrayList<String> toDoItems;
+	private ArrayAdapter<String> aToDoItems;
+	private ListView lvItems;
+	private EditText etNewItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do);
+        
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        etNewItem = (EditText) findViewById(R.id.etNewItem);
+        
+        readItems();
+        aToDoItems = new ArrayAdapter<String>(
+        		this,
+        		android.R.layout.simple_list_item_1,
+        		toDoItems);
+        lvItems.setAdapter(aToDoItems);
+        setupListViewListener();
+    }
+    
+    private void readItems() {
+    	toDoItems = new ArrayList<String>();
+    	toDoItems.add("Item 1");
+    	toDoItems.add("Item 2");
+    	toDoItems.add("Item 3");
     }
 
+    private void writeItems() {
+    	
+    }
+    
+    /*
+     * Corresponds to btnNewItem, add item from etNewItem to lvItems
+     */
+    public void onAddedItem(View v) {
+    	String itemText = etNewItem.getText().toString();
+    	aToDoItems.add(itemText);
+    	etNewItem.setText("");
+    	
+    }
+    
+    private void setupListViewListener() {
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener() {
+			@Override
+			public boolean onItemLongClick(AdapterView<?> adapter, View item,
+					int pos, long id) {
+				toDoItems.remove(pos);
+				aToDoItems.notifyDataSetChanged();
+				return true;
+			}
+		});
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,5 +74,4 @@ public class ToDoActivity extends Activity {
         getMenuInflater().inflate(R.menu.to_do, menu);
         return true;
     }
-    
 }
