1868b7e82f704df5bbc33838a6dbcb4fa421b4d4
ryan <ryanwhitworth@sbcglobal.net>
2013-12-14 16:33:37 -0800

    removed unused import

--- SimpleToDo/src/codepath/app/simpletodo/ToDoActivity.java
+++ SimpleToDo/src/codepath/app/simpletodo/ToDoActivity.java
@@ -21,7 +21,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ToDoActivity extends Activity {
 	private ArrayList<String> toDoItems;
